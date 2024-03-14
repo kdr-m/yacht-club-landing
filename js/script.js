ce9cca18d460d614a2ba794384cdaeac6e8f194c
Kadriya <kadriya-2000@yandex.ru>
2024-03-14 10:18:06 +0300

    Учебный проект по курсу Веб-технологии на HTMLacademy

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let darkThemeButton = document.querySelector('.theme-button-dark');
+let lightThemeButton = document.querySelector('.theme-button-light');
+let sansSerifFontButton = document.querySelector('.font-button-sans-serif');
+let serifFontButton = document.querySelector('.font-button-serif');
+let articleSection = document.querySelector('.club-article.short')
+
+darkThemeButton.onclick = function () {
+    document.body.classList.add('dark');
+    darkThemeButton.classList.add('active');
+    lightThemeButton.classList.remove('active');
+};
+
+lightThemeButton.onclick = function () {
+    document.body.classList.remove('dark');
+    lightThemeButton.classList.add('active');
+    darkThemeButton.classList.remove('active');
+};
+
+serifFontButton.onclick = function () {
+    document.body.classList.add('serif');
+    sansSerifFontButton.classList.remove('active');
+    serifFontButton.classList.add('active');
+};
+
+sansSerifFontButton.onclick = function () {
+   document.body.classList.remove('serif');
+   sansSerifFontButton.classList.add('active');
+   serifFontButton.classList.remove('active');
+};
+
+for (let articleSection of articleSection) {
+    let moreButton = articleSection.querySelector('.more');
+    moreButton.onclick = function () {
+        articleSection.classList.remove('short');
+    };
+};
+
